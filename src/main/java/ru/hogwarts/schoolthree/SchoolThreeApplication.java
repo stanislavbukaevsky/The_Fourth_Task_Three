7976d5720337536fec6700581ecd3884e087d6d0
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2022-12-02 15:13:52 +0300

    Первый коммит

---
+++ src/main/java/ru/hogwarts/schoolthree/SchoolThreeApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwarts/schoolthree/SchoolThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolThreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
